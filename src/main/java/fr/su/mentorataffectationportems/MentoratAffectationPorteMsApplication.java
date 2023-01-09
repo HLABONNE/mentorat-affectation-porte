79d4d9e30401c966120783adc64b22f76066dd13
Hervé LABONNE <herve.labonne@systeme-u.fr>
2023-01-09 17:11:11 +0100

    first commit

---
+++ src/main/java/fr/su/mentorataffectationportems/MentoratAffectationPorteMsApplication.java
@@ -0,0 +1,13 @@
+package fr.su.mentorataffectationportems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoratAffectationPorteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoratAffectationPorteMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/su/mentorataffectationportems/MentoratAffectationPorteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.su.mentorataffectationportems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoratAffectationPorteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
